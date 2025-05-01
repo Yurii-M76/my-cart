6a9157bb52f0eb670a3e2327b7ca2680b5f0748b
Yurii M <ispoyar@yandex.ru>
2025-05-01 16:56:09 +0300

    modified CardUI

--- components/ui/card/CardUI.tsx
+++ components/ui/card/CardUI.tsx
@@ -1,28 +1,28 @@
+"use client";
 import { FC } from "react";
+import { useRouter } from "next/navigation";
+import { TCard } from "@/types";
 import classes from "./card.module.css";
 
-type TCardUI = {
-  label: string;
-  icon: string;
-  count: number;
-};
+const CardUI: FC<TCard> = ({ label, icon, count, path }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(path);
+  };
 
-const CardUI: FC<TCardUI> = ({ label, icon, count }) => {
   return (
-    <div className="section">
-      <div className={classes.card}>
-        <div
-          className={classes.icon}
-          style={{ backgroundImage: `url(${icon})` }}
-        ></div>
-        <div className={classes.label}>{label}</div>
-        {count > 0 && (
-          <div className={classes.count}>
-            {count}
-            <span className={classes.marker}></span>
-          </div>
-        )}
-      </div>
+    <div className={classes.card} onClick={handleClick}>
+      <div
+        className={classes.icon}
+        style={{ backgroundImage: `url(${icon})` }}
+      ></div>
+      <div className={classes.label}>{label}</div>
+      {count > 0 && (
+        <div className={classes.count}>
+          {count}
+          <span className={classes.marker}></span>
+        </div>
+      )}
     </div>
   );
 };
