029716a5e72bced5eaa8cc9f9f1cfb52b565c3f9
Yurii M <ispoyar@yandex.ru>
2025-05-02 10:52:03 +0300

    added BackButtonUI to PageUI

--- components/ui/page/PageUI.tsx
+++ components/ui/page/PageUI.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { BackButtonUI } from "../buttons";
 import { TPage } from "@/types";
 import classes from "./page.module.css";
 
@@ -7,7 +8,7 @@ const PageUI: FC<TPage> = ({ title, children }) => {
     <div className="section">
       <div className={classes.pageHeader}>
         <h2>{title}</h2>
-        <div>back</div>
+        <BackButtonUI label="Назад" />
       </div>
       {children}
     </div>
