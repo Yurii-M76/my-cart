07f4a140bbcf0bd209cabc41b3383dd6c43ca3ad
Yurii M <ispoyar@yandex.ru>
2025-05-02 16:50:13 +0300

    create SaveButtonUI

---
+++ components/ui/buttons/save/SaveButtonUI.tsx
@@ -0,0 +1,11 @@
+import classes from "./save-button.module.css";
+
+const SaveButtonUI = () => {
+  return (
+    <button type="button" className={classes.save}>
+      Сохранить
+    </button>
+  );
+};
+
+export default SaveButtonUI;
