f2f5c94ca93e88fedfdfb580f411dc7ea516d082
Yurii M <ispoyar@yandex.ru>
2025-05-03 15:05:28 +0300

    added CatalogUI

--- components/ui/catalog/CatalogUI.tsx
+++ components/ui/catalog/CatalogUI.tsx
@@ -1,20 +1,30 @@
-import { AddButtonUI } from "../buttons";
+import { FC } from "react";
+import { AddButtonUI, SaveButtonUI } from "../buttons";
+import { ProductListUI } from "../product-list";
+import { PriceUI } from "../price";
+import { TCatalog } from "@/types";
 import classes from "./catalog.module.css";
 
-const CatalogUI = () => {
+const CatalogUI: FC<TCatalog> = ({ items }) => {
   return (
-    <>
-      <div className={classes.list}>list</div>
+    <div className={classes.catalog}>
+      <div className={classes.list}>
+        <ProductListUI items={items} />
+      </div>
       <div className={classes.bottom}>
         <div className={classes.button}>
           <AddButtonUI />
         </div>
         <div className={classes.cart}>
-          <div>price</div>
-          <div>save</div>
+          <PriceUI
+            variant="cart"
+            total={1223.9}
+            items={{ count: 3, total: 11 }}
+          />
+          <SaveButtonUI />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
