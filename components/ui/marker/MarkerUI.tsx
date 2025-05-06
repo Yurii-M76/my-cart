86ebbcd76e148b2d0e03573b674a904d338d1358
Yurii M <ispoyar@yandex.ru>
2025-05-06 15:40:10 +0300

    added style prop to MarkerUI

--- components/ui/marker/MarkerUI.tsx
+++ components/ui/marker/MarkerUI.tsx
@@ -2,12 +2,12 @@ import { FC } from "react";
 import { TMarker } from "@/types";
 import classes from "./marker.module.css";
 
-const MarkerUI: FC<TMarker> = ({ color, size }) => {
+const MarkerUI: FC<TMarker> = ({ color, size, style }) => {
   const classesList = [classes.marker, classes[color], classes[size]]
     .filter(Boolean)
     .join(" ");
 
-  return <span className={classesList} />;
+  return <span className={classesList} style={style} />;
 };
 
 export default MarkerUI;
