7a428411a67399efde8a767a31aadc81fa0f831d
Yurii M <ispoyar@yandex.ru>
2025-05-06 15:57:34 +0300

    create FavoritesButtonUI

---
+++ components/ui/buttons/favorites/index.ts
@@ -0,0 +1,1 @@
+export { default as FavoritesButtonUI } from "./FavoritesButtonUI";
--- components/ui/buttons/index.ts
+++ components/ui/buttons/index.ts
@@ -1,3 +1,4 @@
 export * from "./add";
 export * from "./back";
 export * from "./save";
+export * from "./favorites";
