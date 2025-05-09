d16141340af47dfc03d391549b83a7a6416bbc8e
Yurii M <ispoyar@yandex.ru>
2025-05-09 15:16:09 +0300

    create ActionIcon

---
+++ components/ui/action-icon/ActionIconUI.tsx
@@ -0,0 +1,45 @@
+import { CSSProperties, FC, ReactNode } from "react";
+import classes from "./action-icon.module.css";
+
+type TActionIconUI = {
+  children: ReactNode;
+  size: "sm" | "md" | "lg" | "xl";
+  variant: "circle" | "box";
+  onClick: () => void;
+  shadow?: boolean;
+  style?: CSSProperties | undefined;
+  disabled?: boolean;
+};
+
+const ActionIconUI: FC<TActionIconUI> = ({
+  size,
+  variant,
+  shadow,
+  children,
+  onClick,
+  style,
+  disabled,
+}) => {
+  const classNames = [
+    classes.actionIcon,
+    classes[size],
+    classes[variant],
+    shadow ? classes.shadow : null,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <button
+      type="button"
+      className={classNames}
+      onClick={onClick}
+      style={style}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ActionIconUI;
