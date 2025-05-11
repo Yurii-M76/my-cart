8b53f407dd388e8dbab7dd946ccfb688fea31bbc
Yurii M <ispoyar@yandex.ru>
2025-05-11 16:31:25 +0300

    create CartIcon, CheckIcon, PlusIcon, StarIcon

--- components/ui/icons/index.ts
+++ components/ui/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as CartIconUI } from "./CartIcon";
 export { default as StarIconUI } from "./StarIcon";
 export { default as PlusIconUI } from "./PlusIcon";
+export { default as CheckIconUI } from "./CheckIcon";
