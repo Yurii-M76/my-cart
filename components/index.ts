143d7142c0d8214247080980387b285e9295b1bc
Yurii M <ispoyar@yandex.ru>
2025-05-11 16:34:33 +0300

    added product-selection, product-list to index components

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,5 @@ export * from "./profile";
 export * from "./notification";
 export * from "./cards";
 export * from "./catalog";
+export * from "./product-selection";
+export * from "./product-list";
