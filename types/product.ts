515168662631a524b66a478b9e0805ef8da9dffc
Yurii M <ispoyar@yandex.ru>
2025-05-11 16:29:06 +0300

    fixed product types

--- types/product.ts
+++ types/product.ts
@@ -1,6 +1,15 @@
+type TProductId = string;
+
 export type TProduct = {
-  id: string;
+  id: TProductId;
   label: string;
   price: number;
   category: string;
+};
+
+export type TProductSelected = { id: TProductId; count: number };
+
+export type TProductSelection = {
+  productId: TProductId;
+  setSelected: ({ id, count }: TProductSelected) => void;
 };
