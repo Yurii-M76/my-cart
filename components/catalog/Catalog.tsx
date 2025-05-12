acc755d8ed491aff2f1e7596f0e011f05606dd21
Yurii M <ispoyar@yandex.ru>
2025-05-12 15:31:05 +0300

    fixed Catalog

--- components/catalog/Catalog.tsx
+++ components/catalog/Catalog.tsx
@@ -2,16 +2,23 @@
 import { CatalogUI } from "../ui";
 import { useSelector } from "@/store/store";
 import { getProducts } from "@/store/productsSlice";
+import { getSelectedProducts } from "@/store/selectedProductsSlice";
 
 const Catalog = () => {
   const products = useSelector(getProducts);
+  const selectedItems = useSelector(getSelectedProducts);
+
+  const totalPrice = selectedItems.reduce((acc, item) => {
+    const product = products.find((product) => product.id === item.id);
+    return acc + (product ? product.price * item.count : 0);
+  }, 0);
 
   return (
     <CatalogUI
       items={products}
       totalItems={products.length}
-      totalPrice={"0"}
-      selected={0}
+      totalPrice={totalPrice.toLocaleString()}
+      selected={selectedItems.length}
     />
   );
 };
