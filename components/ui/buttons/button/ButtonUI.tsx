4ae190056b57994d77c409437dc52b434b51bba7
Yurii M <ispoyar@yandex.ru>
2025-05-15 13:12:15 +0300

    create ButtonUI

---
+++ components/ui/buttons/button/ButtonUI.tsx
@@ -0,0 +1,44 @@
+import { CSSProperties, FC } from "react";
+import classes from "./button.module.css";
+
+type TButtonUI = {
+  type: "button" | "submit" | "reset";
+  label: string;
+  onClick: () => void;
+  size?: "sm" | "md" | "lg" | "full";
+  color?: "carrot" | "blue" | "gray";
+  disabled?: boolean;
+  style?: CSSProperties | undefined;
+};
+
+const ButtonUI: FC<TButtonUI> = ({
+  type,
+  label,
+  size,
+  color,
+  onClick,
+  disabled,
+  style,
+}) => {
+  const classNames = [
+    classes.button,
+    size ? classes[size] : classes.full,
+    color ? classes[color] : classes.blue,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={classNames}
+      style={style}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default ButtonUI;
--- components/ui/buttons/index.ts
+++ components/ui/buttons/index.ts
@@ -1,2 +1,3 @@
+export * from "./button";
 export * from "./back";
 export * from "./save";
