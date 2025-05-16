20a29bb74e3f4d6f2a1c1ab7dd31134eca3453b6
Yurii M <ispoyar@yandex.ru>
2025-05-16 15:28:09 +0300

    create HomeIconUI

--- components/ui/icons/index.ts
+++ components/ui/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as HomeIconUI } from "./HomeIcon";
 export { default as CartIconUI } from "./CartIcon";
 export { default as StarIconUI } from "./StarIcon";
 export { default as PlusIconUI } from "./PlusIcon";
