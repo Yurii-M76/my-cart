37af730af82a9cdd1ef499a6c2b8362cfb967c09
Yurii M <ispoyar@yandex.ru>
2025-05-16 15:42:26 +0300

    added ActionIconUI & ProfileIconUI to Profile

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,7 +1,24 @@
-import { ProfileUI } from "../ui";
+"use client";
+import { useRouter } from "next/navigation";
+import { ActionIconUI, ProfileIconUI } from "../ui";
 
 const Profile = () => {
-  return <ProfileUI />;
+  const router = useRouter();
+  return (
+    <ActionIconUI
+      size="md"
+      variant="box-br-12"
+      color="light-blue"
+      onClick={() => router.push("#")}
+    >
+      <ProfileIconUI
+        width={24}
+        height={24}
+        strokeWidth="0"
+        fill="currentColor"
+      />
+    </ActionIconUI>
+  );
 };
 
 export default Profile;
