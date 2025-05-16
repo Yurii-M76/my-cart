8d0da0c65c9d12dfd065db8d2192801e64238e4f
Yurii M <ispoyar@yandex.ru>
2025-05-16 15:52:32 +0300

    added ActionIconUI & BellIconUI to Notification

--- components/notification/Notification.tsx
+++ components/notification/Notification.tsx
@@ -1,7 +1,19 @@
-import { NotificationUI } from "../ui";
+"use client";
+import { useRouter } from "next/navigation";
+import { ActionIconUI, BellIconUI } from "../ui";
 
 const Notification = () => {
-  return <NotificationUI />;
+  const router = useRouter();
+  return (
+    <ActionIconUI
+      variant="box-br-12"
+      color="transparent"
+      size="md"
+      onClick={() => router.push("#")}
+    >
+      <BellIconUI width={24} height={24} />
+    </ActionIconUI>
+  );
 };
 
 export default Notification;
