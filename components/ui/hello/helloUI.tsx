e41c59df1a9cbc6cacf8964cc781c44a55d99c61
Yurii M <ispoyar@yandex.ru>
2025-05-16 15:29:08 +0300

    added ActionIconUI & fixed styles to HelloUI

--- components/ui/hello/helloUI.tsx
+++ components/ui/hello/helloUI.tsx
@@ -1,17 +1,30 @@
 import { FC } from "react";
+import { ActionIconUI } from "../action-icon";
+import { HomeIconUI } from "../icons";
 import classes from "./hello.module.css";
 
 type THelloUI = {
   userName: string;
   welcomeText: string;
+  onClickToHome: () => void;
 };
 
-const HelloUI: FC<THelloUI> = ({ userName, welcomeText }) => {
+const HelloUI: FC<THelloUI> = ({ userName, welcomeText, onClickToHome }) => {
   return (
-    <>
-      <span className={classes.welcomeText}>{welcomeText},</span>
-      <span className={classes.userName}>{userName}</span>
-    </>
+    <div className={classes.hello}>
+      <ActionIconUI
+        size="md"
+        variant="box-br-12"
+        color="dark"
+        onClick={onClickToHome}
+      >
+        <HomeIconUI width={24} height={24} fill="#ffffff" />
+      </ActionIconUI>
+      <div className={classes.welcome}>
+        <span className={classes.welcomeText}>{welcomeText},</span>
+        <span className={classes.userName}>{userName}</span>
+      </div>
+    </div>
   );
 };
 
