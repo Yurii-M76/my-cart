a9d9284e29a10321cf8c89409a960df7d6f26138
Yurii M <ispoyar@yandex.ru>
2025-05-20 15:42:56 +0300

    modified Catalog

--- components/catalog/Catalog.tsx
+++ components/catalog/Catalog.tsx
@@ -1,26 +1,54 @@
 "use client";
-import { CatalogUI } from "../ui";
-import { useSelector } from "@/store/store";
-import { getProducts } from "@/store/productsSlice";
-import { getSelectedProducts } from "@/store/selectedProductsSlice";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "@/services/store";
+import { getProducts, loading } from "@/services/products/productsSlice";
+import { getSelectedProducts } from "@/services/selectedProductsSlice";
+import { findProducts } from "@/services/products/actions";
 import { ProductList } from "../product-list";
+import { Modal } from "../modal";
+import { SaveShoppingListForm } from "../forms";
+import { CatalogUI, LoaderUI } from "../ui";
 
 const Catalog = () => {
+  const dispatch = useDispatch();
   const products = useSelector(getProducts);
+  const isLoading = useSelector(loading);
   const selectedItems = useSelector(getSelectedProducts);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const router = useRouter();
 
   const totalPrice = selectedItems.reduce((acc, item) => {
     const product = products.find((product) => product.id === item.id);
     return acc + (product ? product.price * item.count : 0);
   }, 0);
 
+  useEffect(() => {
+    dispatch(findProducts());
+  }, [dispatch]);
+
   return (
     <CatalogUI
       totalItems={products.length}
       totalPrice={totalPrice.toLocaleString()}
       selectedLength={selectedItems.length}
+      onSave={() => setIsOpenModal(true)}
+      onProductCreate={() => router.push("/catalog/new-product")}
     >
-      <ProductList selectedItems={selectedItems} items={products} />
+      <Modal
+        title="Сохранить список покупок"
+        isOpen={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+      >
+        <h4>Сумма: {totalPrice.toLocaleString()} ₽</h4>
+        <SaveShoppingListForm onCancel={() => setIsOpenModal(false)} />
+      </Modal>
+
+      {isLoading ? (
+        <LoaderUI color="gray" />
+      ) : (
+        <ProductList selectedItems={selectedItems} items={products} />
+      )}
     </CatalogUI>
   );
 };
