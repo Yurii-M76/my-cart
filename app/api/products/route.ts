48fb78925594548aa3420dd505cfd5878876b830
Yurii M <ispoyar@yandex.ru>
2025-05-25 16:24:25 +0300

    fixed route to products endpoint

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,15 +1,22 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@/generated/prisma";
-import { TProduct } from "@/types";
 
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
     const response = await prisma.products.findMany({
-      omit: {
-        createdAt: true,
-        updatedAt: true,
+      select: {
+        id: true,
+        label: true,
+        description: true,
+        price: true,
+        category: {
+          select: {
+            id: true,
+            label: true,
+          },
+        },
       },
     });
     return NextResponse.json(response);
@@ -21,13 +28,29 @@ export async function GET() {
 
 export async function POST(req: Request) {
   try {
-    const body = (await req.json()) as TProduct;
+    const body = await req.json();
     const response = await prisma.products.create({
       data: {
         label: body.label,
         description: body.description,
-        categoryId: body.categoryId,
+        category: {
+          connect: {
+            id: body.categoryId,
+          },
+        },
         price: body.price,
+      },
+      select: {
+        id: true,
+        label: true,
+        description: true,
+        price: true,
+        category: {
+          select: {
+            id: true,
+            label: true,
+          },
+        },
       },
     });
     return NextResponse.json(response);
