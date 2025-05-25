86cf93e1bc2613718a56e402131b8ff114470733
Yurii M <ispoyar@yandex.ru>
2025-05-25 16:35:15 +0300

    fixed SaveProductForm

--- components/forms/save-product/SaveProductForm.tsx
+++ components/forms/save-product/SaveProductForm.tsx
@@ -1,60 +1,86 @@
 "use client";
-import { useEffect } from "react";
+import { FC, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "@/services/store";
 import { createProduct } from "@/services/products/actions";
 import { loading } from "@/services/products/productsSlice";
+import { findProductCategories } from "@/services/product-categories/actions";
+import { getProductCategories } from "@/services/product-categories/productCategoriesSlice";
 import { ButtonUI, InputUI, SelectUI, TextAreaUI } from "@/components/ui";
 import { exceptions } from "@/constants";
+import { TProduct } from "@/types";
 
 type TInitialState = {
   productName: string;
   description: string;
-  category: string;
+  categorуId: string;
   price: number;
 };
 
-const category = [
-  { value: "value1", label: "Electronics" },
-  { value: "value2", label: "Fashion" },
-  { value: "value3", label: "Home Goods" },
-];
+type TSaveProductForm = {
+  updData?: TProduct;
+};
 
-const SaveProductForm = () => {
+const SaveProductForm: FC<TSaveProductForm> = ({ updData }) => {
   const dispatch = useDispatch();
   const router = useRouter();
   const isLoading = useSelector(loading);
+  const categories = useSelector(getProductCategories);
+
   const initialValues: TInitialState = {
     productName: "",
     description: "",
-    category: "",
+    categorуId: "",
     price: 0,
   };
 
-  const { register, getValues, formState, reset, handleSubmit } =
+  const { register, getValues, setValue, formState, reset, handleSubmit } =
     useForm<TInitialState>({
       defaultValues: initialValues,
     });
 
   const { isDirty, errors, isSubmitSuccessful } = formState;
 
-  const onSubmit = () => {
+  const onSaveHandler = () => {
     const newProduct = {
       label: getValues("productName"),
       description: getValues("description"),
+      categoryId: getValues("categorуId"),
       price: +getValues("price"),
     };
 
     dispatch(createProduct(newProduct));
   };
 
+  const onUpdateHandler = () => {
+    console.log("update");
+  };
+  const onDeleteHandler = () => {
+    console.log("delete");
+  };
+
+  useEffect(() => {
+    if (updData) {
+      setValue("productName", updData.label);
+      setValue("description", updData.description ?? "");
+      setValue("price", updData.price);
+    }
+  }, [setValue, updData]);
+
   useEffect(() => {
     if (isSubmitSuccessful && !isLoading) router.push("/catalog");
   }, [isLoading, isSubmitSuccessful, router]);
 
+  useEffect(() => {
+    dispatch(findProductCategories());
+  }, [dispatch]);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} onReset={() => reset()}>
+    <form
+      onSubmit={handleSubmit(updData ? onUpdateHandler : onSaveHandler)}
+      onReset={() => reset()}
+    >
       <InputUI
         type="text"
         label="Наименование"
@@ -71,11 +97,14 @@ const SaveProductForm = () => {
       />
       <SelectUI
         label="Категория"
-        data={category}
+        data={categories.map((item) => ({
+          value: item.id,
+          label: item.label,
+        }))}
         size="lg"
         key={"category"}
-        {...register("category", { required: exceptions.form.required })}
-        error={errors.category?.message}
+        {...register("categorуId", { required: exceptions.form.required })}
+        error={errors.categorуId?.message}
       />
       <InputUI
         type="number"
@@ -86,7 +115,17 @@ const SaveProductForm = () => {
         error={errors.price?.message}
       />
 
-      <div className="formButtons">
+      <div className={`formButtons ${updData && "right"}`}>
+        {updData && (
+          <ButtonUI
+            type="button"
+            label="Удалить"
+            size="sm"
+            color="red"
+            variant="transparent"
+            onClick={onDeleteHandler}
+          />
+        )}
         <ButtonUI
           type="reset"
           label="Очистить"
@@ -96,8 +135,8 @@ const SaveProductForm = () => {
         />
         <ButtonUI
           type="submit"
-          label="Сохранить"
-          color="blue"
+          label={updData ? "Обновить" : "Сохранить"}
+          color="light-blue"
           size="sm"
           disabled={!isDirty}
           isLoading={isLoading}
