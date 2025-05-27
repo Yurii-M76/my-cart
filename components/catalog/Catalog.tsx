9958680e81d99381da13229f7cb251fa7dd83e4a
Yurii M <ispoyar@yandex.ru>
2025-05-27 14:01:44 +0300

    modified Catalog

--- components/catalog/Catalog.tsx
+++ components/catalog/Catalog.tsx
@@ -1,65 +1,108 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "@/services/store";
-import { getProducts, loading } from "@/services/products/productsSlice";
-import { getSelectedProducts } from "@/services/selectedProductsSlice";
 import { findProducts } from "@/services/products/actions";
+import { findProductCategories } from "@/services/product-categories/actions";
+import {
+  getErrors,
+  getProducts,
+  loading,
+  resetErrors,
+  success,
+} from "@/services/products/productsSlice";
+import { getSelectedProducts } from "@/services/selectedProductsSlice";
+import { getProductCategories } from "@/services/product-categories/productCategoriesSlice";
 import { ProductList } from "../product-list";
 import { Modal } from "../modal";
-import { SaveShoppingListForm } from "../forms";
-import { CatalogUI, LoaderUI } from "../ui";
+import { SaveProductForm, SaveShoppingListForm } from "../forms";
+import { CatalogUI, LoaderUI, SaveShoppingListUI } from "../ui";
 
 const Catalog = () => {
   const dispatch = useDispatch();
   const products = useSelector(getProducts);
+  const productsSelected = useSelector(getSelectedProducts);
+  const productCategories = useSelector(getProductCategories);
   const isLoading = useSelector(loading);
-  const selectedItems = useSelector(getSelectedProducts);
-  const router = useRouter();
-  const [modalSaveFormIsOpen, setModalSaveFormIsOpen] = useState(false);
-  const [modalEditFormIsOpen, setModalEditFormIsOpen] = useState(false);
+  const isSuccess = useSelector(success);
+  const errors = useSelector(getErrors);
 
-  const totalPrice = selectedItems.reduce((acc, item) => {
+  const [showModalSaveCart, setShowModalSaveCart] = useState(false);
+  const [showModalSaveProduct, setShowModalSaveProduct] = useState(false);
+  const [productIdToUpdate, setProductIdToUpdate] = useState<
+    string | null | undefined
+  >(null);
+
+  const totalPrice = productsSelected.reduce((acc, item) => {
     const product = products.find((product) => product.id === item.id);
     return acc + (product ? product.price * item.count : 0);
   }, 0);
 
+  const productToUpdate = useMemo(() => {
+    if (productIdToUpdate === null) return undefined;
+    const product = products.find((item) => item.id === productIdToUpdate);
+    if (product) setShowModalSaveProduct(true);
+    return product;
+  }, [products, productIdToUpdate, setShowModalSaveProduct]);
+
+  const productsSelectedCount = productsSelected.length;
+
   useEffect(() => {
     dispatch(findProducts());
+    dispatch(findProductCategories());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (!isLoading) setProductIdToUpdate(null);
+  }, [isLoading, setProductIdToUpdate]);
+
+  useEffect(() => {
+    if (isSuccess) setShowModalSaveProduct(false);
+  }, [isSuccess, setShowModalSaveProduct]);
 
   return (
     <CatalogUI
       totalItems={products.length}
       totalPrice={totalPrice.toLocaleString()}
-      selectedLength={selectedItems.length}
-      onSave={() => setModalSaveFormIsOpen(true)}
-      onProductCreate={() => router.push("/catalog/new-product")}
+      selectedLength={productsSelectedCount}
+      onSaveCart={() => setShowModalSaveCart(true)}
+      onProductCreate={() => setShowModalSaveProduct(true)}
     >
       <Modal
-        title="Сохранить список покупок"
-        isOpen={modalSaveFormIsOpen}
-        onClose={() => setModalSaveFormIsOpen(false)}
+        title="Сохранить список"
+        isOpen={showModalSaveCart}
+        onClose={() => setShowModalSaveCart(false)}
       >
-        <h4>Сумма: {totalPrice.toLocaleString()} ₽</h4>
-        <SaveShoppingListForm />
+        <SaveShoppingListUI
+          totalPrice={totalPrice.toLocaleString()}
+          productsCount={productsSelectedCount}
+        >
+          <SaveShoppingListForm />
+        </SaveShoppingListUI>
       </Modal>
 
       <Modal
-        title="Редактировать"
-        isOpen={modalEditFormIsOpen}
-        onClose={() => setModalEditFormIsOpen(false)}
+        title={showModalSaveProduct ? "Новый продукт" : "Редактировать"}
+        isOpen={showModalSaveProduct}
+        onClose={() => {
+          setProductIdToUpdate(null);
+          setShowModalSaveProduct(false);
+          dispatch(resetErrors());
+        }}
       >
-        content
+        <SaveProductForm
+          updData={productToUpdate}
+          categories={productCategories}
+          errorMessage={errors}
+        />
       </Modal>
 
       {isLoading ? (
         <LoaderUI color="gray" />
       ) : (
         <ProductList
-          selectedItems={selectedItems}
           items={products}
-          onOpenModal={() => setModalEditFormIsOpen(true)}
+          selectedItems={productsSelected}
+          onClickItem={setProductIdToUpdate}
         />
       )}
     </CatalogUI>
