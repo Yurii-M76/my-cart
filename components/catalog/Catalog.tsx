9c44117278d62eefe6223610a6c804e6c3872f28
Yurii M <ispoyar@yandex.ru>
2025-05-30 16:45:55 +0300

    modified Catalog

--- components/catalog/Catalog.tsx
+++ components/catalog/Catalog.tsx
@@ -1,20 +1,35 @@
 "use client";
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "@/services/store";
-import { findProducts } from "@/services/products/actions";
+import {
+  createProduct,
+  deleteProduct,
+  findProducts,
+  updateProduct,
+} from "@/services/products/actions";
 import { findProductCategories } from "@/services/product-categories/actions";
 import {
   getProducts,
   getProductsError,
   getProductsStatus,
+  getProductToUpdate,
   resetErrors,
+  resetProductToUpdate,
+  setProductToUpdate,
 } from "@/services/products/productsSlice";
 import { getSelectedProducts } from "@/services/selectedProductsSlice";
 import { getProductCategories } from "@/services/product-categories/productCategoriesSlice";
 import { ProductList } from "../product-list";
 import { Modal } from "../modal";
 import { SaveProductForm, SaveShoppingListForm } from "../forms";
-import { AlertUI, CatalogUI, LoaderUI, SaveShoppingListUI } from "../ui";
+import {
+  AlertUI,
+  ButtonUI,
+  CatalogUI,
+  LoaderUI,
+  SaveShoppingListUI,
+} from "../ui";
+import { TProductUpdate, TSaveProduct } from "@/types";
 
 const Catalog = () => {
   const dispatch = useDispatch();
@@ -23,26 +38,82 @@ const Catalog = () => {
   const errors = useSelector(getProductsError);
   const productsSelected = useSelector(getSelectedProducts);
   const productCategories = useSelector(getProductCategories);
+  const productToUpdate = useSelector(getProductToUpdate);
 
   const [showModalSaveCart, setShowModalSaveCart] = useState(false);
-  const [showModalSaveProduct, setShowModalSaveProduct] = useState(false);
+  const [showModalProductSave, setShowModalProductSave] = useState(false);
   const [productIdToUpdate, setProductIdToUpdate] = useState<
     string | null | undefined
   >(null);
+  const [isDelete, setIsDelete] = useState(false);
 
   const totalPrice = productsSelected.reduce((acc, item) => {
     const product = products.find((product) => product.id === item.id);
     return acc + (product ? product.price * item.count : 0);
   }, 0);
 
-  const productToUpdate = useMemo(() => {
-    if (productIdToUpdate === null) return undefined;
-    const product = products.find((item) => item.id === productIdToUpdate);
-    if (product) setShowModalSaveProduct(true);
-    return product;
-  }, [products, productIdToUpdate, setShowModalSaveProduct]);
-
   const productsSelectedCount = productsSelected.length;
+
+  const productSaveHandler = (data: TSaveProduct) => {
+    dispatch(createProduct(data));
+  };
+
+  const productUpdateHandler = (data: TProductUpdate) => {
+    dispatch(updateProduct(data));
+  };
+
+  const productDeleteHandler = () => {
+    if (productIdToUpdate) {
+      dispatch(deleteProduct(productIdToUpdate));
+    }
+  };
+
+  const productUpdateCloseHandler = useCallback(() => {
+    setProductIdToUpdate(null);
+    setShowModalProductSave(false);
+    setIsDelete(false);
+    dispatch(resetProductToUpdate())
+    if (errors) {
+      dispatch(resetErrors());
+    }
+  }, [dispatch, errors]);
+
+  const confirmDeleteAction = (
+    <>
+      <AlertUI icon variant="transparent">
+        <p>
+          Вы собираетесь удалить <strong>{productToUpdate?.label}</strong>. Это
+          действие нельзя отменить.
+        </p>
+      </AlertUI>
+      <div className="buttonGroup">
+        <ButtonUI
+          type="button"
+          label="Отменить"
+          color="light-gray"
+          onClick={() => setIsDelete(false)}
+        />
+        <ButtonUI
+          type="button"
+          label="Удалить"
+          color="red"
+          onClick={productDeleteHandler}
+          isLoading={status.delete.loading}
+        />
+      </div>
+    </>
+  );
+
+  const noData = (
+    <AlertUI icon color="light-gray" variant="filled">
+      <p>
+        Нет данных -{" "}
+        <a href="#" onClick={() => setShowModalProductSave(true)}>
+          добавить
+        </a>
+      </p>
+    </AlertUI>
+  );
 
   useEffect(() => {
     dispatch(findProducts());
@@ -50,15 +121,32 @@ const Catalog = () => {
   }, [dispatch]);
 
   useEffect(() => {
-    if (status.update.success) {
-      setProductIdToUpdate(null);
-      setShowModalSaveProduct(false);
+    if (status.create.success) {
+      setShowModalProductSave(false);
     }
+    if (errors) {
+      resetErrors();
+    }
+  }, [errors, status.create.success]);
 
-    if (status.create.success || status.delete.success) {
-      setShowModalSaveProduct(false);
+  useEffect(() => {
+    if (productIdToUpdate) {
+      setShowModalProductSave(true);
+      dispatch(setProductToUpdate(productIdToUpdate));
     }
-  }, [status.create.success, status.delete.success, status.update.success]);
+  }, [dispatch, productIdToUpdate]);
+
+  useEffect(() => {
+    if (status.update.success) {
+      productUpdateCloseHandler();
+    }
+  }, [productUpdateCloseHandler, status.update.success]);
+
+  useEffect(() => {
+    if (status.delete.success) {
+      productUpdateCloseHandler();
+    }
+  }, [productUpdateCloseHandler, status.delete.success]);
 
   return (
     <CatalogUI
@@ -66,7 +154,7 @@ const Catalog = () => {
       totalPrice={totalPrice.toLocaleString()}
       selectedLength={productsSelectedCount}
       onSaveCart={() => setShowModalSaveCart(true)}
-      onProductCreate={() => setShowModalSaveProduct(true)}
+      onProductCreate={() => setShowModalProductSave(true)}
     >
       <Modal
         title="Сохранить список"
@@ -82,31 +170,34 @@ const Catalog = () => {
       </Modal>
 
       <Modal
-        title={productIdToUpdate ? "Редактировать" : "Новый продукт"}
-        isOpen={showModalSaveProduct}
-        onClose={() => {
-          setProductIdToUpdate(null);
-          setShowModalSaveProduct(false);
-          dispatch(resetErrors());
-        }}
+        title={
+          isDelete
+            ? " Подтвердите удаление "
+            : productIdToUpdate
+            ? "Редактировать"
+            : "Новый продукт"
+        }
+        isOpen={showModalProductSave}
+        onClose={productUpdateCloseHandler}
       >
-        <SaveProductForm
-          products={products}
-          updData={productToUpdate}
-          categories={productCategories}
-          errorMessage={errors}
-        />
+        {isDelete ? (
+          confirmDeleteAction
+        ) : (
+          <SaveProductForm
+            status={status}
+            products={products}
+            updData={productToUpdate}
+            categories={productCategories}
+            errorMessage={errors}
+            onSave={productSaveHandler}
+            onUpdate={productUpdateHandler}
+            onDelete={() => setIsDelete(true)}
+          />
+        )}
       </Modal>
 
-      {!products.length ? (
-        <AlertUI icon color="light-gray" variant="filled">
-          <p>
-            Нет данных -{" "}
-            <a href="#" onClick={() => setShowModalSaveProduct(true)}>
-              добавить
-            </a>
-          </p>
-        </AlertUI>
+      {!products.length && status.get.success ? (
+        noData
       ) : status.get.loading ? (
         <LoaderUI color="gray" />
       ) : (
