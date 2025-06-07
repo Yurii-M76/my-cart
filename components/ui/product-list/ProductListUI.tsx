947469063798798c44046de28b939afb50c7af51
Yurii M <ispoyar@yandex.ru>
2025-06-07 14:27:57 +0300

    fixed ProductListUI

--- components/ui/product-list/ProductListUI.tsx
+++ components/ui/product-list/ProductListUI.tsx
@@ -1,50 +1,78 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import { ProductSelection } from "@/components/product-selection";
 import { MarkerUI } from "../marker";
+import { ActionIconUI } from "../action-icon";
+import { StarOfIconUI } from "../icons";
 import { TProduct, TProductSelected } from "@/types";
 import classes from "./product-list.module.css";
 
 type TProductListUI = {
   items: TProduct[];
   selectedItems: TProductSelected[];
+  children: ReactNode;
+  selectedItemsLenght: number;
   onProductSelect: ({ id, count }: TProductSelected) => void;
   onClickItem: (id: string) => void;
+  onResetSelectedItems: () => void;
 };
 
 const ProductListUI: FC<TProductListUI> = ({
   items,
   selectedItems,
+  children,
+  selectedItemsLenght,
   onProductSelect,
   onClickItem,
+  onResetSelectedItems,
 }) => {
   return (
-    <ul className={classes.items}>
-      {items.map((item) => (
-        <li key={item.id} className={classes.item}>
-          <div className={classes.groupRow}>
-            <MarkerUI color="blue" size="xl" style={{ marginTop: "3px" }} />
-            <div className={classes.title}>
-              <div
-                className={classes.label}
-                onClick={() => onClickItem(item.id)}
-              >
-                {item.label}
-              </div>
-              <div className={classes.property}>
-                <span className={classes.price}>{item.price} ₽</span>
-                <span className={classes.category}> • {item.category.label}</span>
+    <div className={classes["product-list"]}>
+      {children}
+      <div className={classes["header-list"]}>
+        <span>
+          <strong>Продукты</strong>
+        </span>
+        <ActionIconUI
+          size="md"
+          variant="circle"
+          shadow
+          onClick={onResetSelectedItems}
+          disabled={!selectedItemsLenght}
+        >
+          <StarOfIconUI />
+        </ActionIconUI>
+      </div>
+      <ul className={classes.items}>
+        {items.map((item) => (
+          <li key={item.id} className={classes.item}>
+            <div className={classes.groupRow}>
+              <MarkerUI color="blue" size="xl" style={{ marginTop: "3px" }} />
+              <div className={classes.title}>
+                <div
+                  className={classes.label}
+                  onClick={() => onClickItem(item.id)}
+                >
+                  {item.label}
+                </div>
+                <div className={classes.property}>
+                  <span className={classes.price}>{item.price} ₽</span>
+                  <span className={classes.category}>
+                    {" "}
+                    • {item.category.label}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
 
-          <ProductSelection
-            productId={item.id}
-            selectedItems={selectedItems}
-            setSelected={onProductSelect}
-          />
-        </li>
-      ))}
-    </ul>
+            <ProductSelection
+              productId={item.id}
+              selectedItems={selectedItems}
+              setSelected={onProductSelect}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
